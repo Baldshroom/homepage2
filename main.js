8e0b3119f641141c91923a79faa60487aa61e21b
Oskar <o.pulczynski@gmail.com>
2024-02-02 20:25:02 +0100

    Added BEM, JavaScript and CSS Styles

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 const firstName = "Oskar";
 const age = "25";
 console.log(`Cześć jestem ${firstName} i mam ${age}lat`);
+
+const heroText = document.querySelector(".header-hero__text--js");
+
+console.log(heroText.innerHTML);
+
+heroText.innerHTML = "hejka!";
+
+console.log(heroText.innerHTML);
