092af5d80fe3cdcde2a252103cde89ac585bde54
Oskar <o.pulczynski@gmail.com>
2024-02-04 13:08:57 +0100

    Added hello and hello2 function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const firstName = "Oskar";
-const age = "25";
-console.log(`Cześć jestem ${firstName} i mam ${age}lat`);
+// const firstName = "Oskar";
+// const age = "25";
+// console.log(`Cześć jestem ${firstName} i mam ${age}lat`);
 
 const heroText = document.querySelector(".header-hero__text--js");
 
@@ -9,3 +9,15 @@ console.log(heroText.innerHTML);
 heroText.innerHTML = "hejka!";
 
 console.log(heroText.innerHTML);
+
+// Pierwsza funkcja
+function hello(firstName, age) {
+  console.log(`Cześć mam na imię ${firstName} i mam ${age} lat`);
+}
+
+function hello2() {
+  firstName2 = 'Oskar';
+  age2 = 25;
+  // return `Cześć mam na imię ${firstName2} i mam ${age2} lat.`;
+  console.log(`Cześć mam na imię ${firstName2} i mam ${age2} lat.`);
+}
